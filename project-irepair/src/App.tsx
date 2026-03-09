3e7eab58f43aaf1eb26df9ba8bfeaca8812f10af
Gabriel Costa <gabrielcunhacosta2005@gmail.com>
2026-03-09 14:03:41 -0300

    feat: Implementa o App

--- project-irepair/src/App.tsx
+++ project-irepair/src/App.tsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import NewServiceForm from './components/NewServiceForm'; 
+import ServiceCard from './components/ServiceCard';
+
+export interface ServiceOrder{
+  id: number;
+  cliente: string;
+  modeloAparelho: string;
+  defeito: string;
+  status: "aberto" | "finalizado";
+}
 
 function App() {
+  const [services, setServices] = useState <ServiceOrder[]> ([]);
+  function addService(newService: ServiceOrder){
+    setServices ([... services, newService]);
+  }
   return (
     <body>
-      <h1 className="text-3xl font-bold">
-      Hello world!
-      </h1>
-      
+      <Header />
+      <main>
+        <NewServiceForm onAddService = {addService} />
+        {services.map((service) => (
+          <ServiceCard key = {service.id} service = {service}/>
+        ))}
+      </main>
     </body>
   )
 }
 
 export default App
+
