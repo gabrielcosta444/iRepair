898aad20c9fbbf137bccc5afebc42facde32556d
Gabriel Costa <gabrielcunhacosta2005@gmail.com>
2026-04-07 14:43:34 -0300

    feat: configuração do CORS e criação da tabela usuario

--- API_Tarefas/src/config/expressConfig.ts
+++ API_Tarefas/src/config/expressConfig.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import { tarefasRoutes } from '../routes/tarefa.routes';
 import { authRoutes } from '../auth/auth.routes';
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
- 
+app.use(cors({
+    origin: 'http://localhost:5173',  // origem exata do seu front-end
+    credentials: true,  
+}))
+
+
 app.use('/tasks', tarefasRoutes);
 app.use('/auth', authRoutes)
 
