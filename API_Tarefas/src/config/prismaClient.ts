2e1c4d266fd88f32a2bea6ce32a2bd092c008cb9
Gabriel Costa <gabrielcunhacosta2005@gmail.com>
2026-04-13 00:30:53 -0300

    fix: prisma client

--- API_Tarefas/src/config/prismaClient.ts
+++ API_Tarefas/src/config/prismaClient.ts
@@ -1,5 +1,5 @@
 // src/config/prismaClient.ts
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "../../node_modules/.prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 const prisma = new PrismaClient({ adapter });
